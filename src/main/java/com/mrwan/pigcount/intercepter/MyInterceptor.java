39a9fad718b3c75171266c340c5dd188422139da
wan.yixin <962717593@qq.com>
2019-07-15 19:05:52 +0800

    签名返回类型修改

--- src/main/java/com/mrwan/pigcount/intercepter/MyInterceptor.java
+++ src/main/java/com/mrwan/pigcount/intercepter/MyInterceptor.java
@@ -2,6 +2,7 @@ package com.mrwan.pigcount.intercepter;
 
 import com.mrwan.pigcount.utils.sign;
 import org.json.JSONObject;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,6 +16,12 @@ import javax.servlet.http.HttpServletResponse;
 public class MyInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse response, Object object) throws Exception {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
         RequestWrapper request = new RequestWrapper(req);
         String check = request.getHeader("Access-Control-Request-Headers");
         String str = request.getBodyString(request);
@@ -22,23 +29,21 @@ public class MyInterceptor implements HandlerInterceptor {
 
         }else {
             if ( str.equals("") ){
-                response.setContentType("text/html;charset=utf-8");
-                response.getWriter().write("我看你就是个蛤蟆皮");
+                response.setStatus(500);
+                response.getWriter().print("我看你就是个蛤蟆皮");
+                response.getWriter();
+                response.getWriter();
                 return false;
             }
             JSONObject test = new JSONObject(str);
             if ( sign.sign_check(test.getString("signature") , test.getString("rand") , test.getInt("timestamp")) ){
-                response.setContentType("text/html;charset=utf-8");
-                response.getWriter().write("超时了");
+                response.setStatus(500);
+                response.getWriter().print("超时了");
+                response.getWriter().flush();
+                response.getWriter().close();
                 return false;
             }
         }
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json; charset=utf-8");
-        response.setHeader("Access-Control-Allow-Credentials","true");
-        response.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
         return true;
     }
 
