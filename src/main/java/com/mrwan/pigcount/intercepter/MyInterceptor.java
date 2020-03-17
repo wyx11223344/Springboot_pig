c8ec6762bbd40ff337a5e76bd89ce9407031d8ad
wan.yixin <962717593@qq.com>
2020-03-17 23:43:10 +0800

    测试

--- src/main/java/com/mrwan/pigcount/intercepter/MyInterceptor.java
+++ src/main/java/com/mrwan/pigcount/intercepter/MyInterceptor.java
@@ -49,6 +49,7 @@ public class MyInterceptor implements HandlerInterceptor {
             if (!noLog){
                 HttpSession uuu = request.getSession();
                 Object users = uuu.getAttribute("username");
+                System.out.println(users);
                 if (users != null) {
                     uuu.setMaxInactiveInterval(1800);
                 } else {
